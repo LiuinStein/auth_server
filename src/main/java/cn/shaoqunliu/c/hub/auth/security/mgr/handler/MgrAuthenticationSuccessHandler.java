f5316d9853996693222440795d277a3fa08a5323
Shaoqun Liu <liuinstein@163.com>
2019-05-08 17:49:33 +0800

    UPDATE: return token and authentication info as Json in response body

--- src/main/java/cn/shaoqunliu/c/hub/auth/security/mgr/handler/MgrAuthenticationSuccessHandler.java
+++ src/main/java/cn/shaoqunliu/c/hub/auth/security/mgr/handler/MgrAuthenticationSuccessHandler.java
@@ -3,6 +3,8 @@ package cn.shaoqunliu.c.hub.auth.security.mgr.handler;
 import cn.shaoqunliu.c.hub.auth.security.common.JwtsUtils;
 import cn.shaoqunliu.c.hub.auth.security.mgr.token.MgrAbstractAuthenticationToken;
 import cn.shaoqunliu.c.hub.auth.vo.MgrAccessDetails;
+import cn.shaoqunliu.c.hub.auth.vo.MgrAuthenticationResult;
+import com.alibaba.fastjson.JSON;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -18,14 +20,18 @@ public class MgrAuthenticationSuccessHandler implements AuthenticationSuccessHan
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_ACCEPTED);
         MgrAccessDetails accessDetails = null;
+        MgrAuthenticationResult authenticationResult = new MgrAuthenticationResult();
         if (authentication instanceof MgrAbstractAuthenticationToken) {
             accessDetails = ((MgrAbstractAuthenticationToken) authentication).getAccessDetails();
+            authenticationResult.setUid(accessDetails.getUid());
+            authenticationResult.setUsername(accessDetails.getUsername());
         }
         String token = JwtsUtils.getBuilder()
                 .claim("access", accessDetails)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 48 * 3600 * 1000))
                 .compact();
-        response.setHeader("Authorization", token);
+        authenticationResult.setToken(token);
+        response.getWriter().println(JSON.toJSONString(authenticationResult));
     }
 }
---
+++ src/main/java/cn/shaoqunliu/c/hub/auth/vo/MgrAuthenticationResult.java
@@ -0,0 +1,32 @@
+package cn.shaoqunliu.c.hub.auth.vo;
+
+public class MgrAuthenticationResult {
+
+    private Integer uid;
+    private String username;
+    private String token;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
